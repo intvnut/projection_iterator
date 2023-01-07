9ad5fffa1cb0dc56052ffe340a77f2981c76d0f7
Joe Zbiciak <intvnut@gmail.com>
2023-01-07 01:58:21 -0800

    Initial commit of the Projection Iterator.
    This header provides an iterator adaptor that applies a projection to a
    random access iterator.  In principle, it can be extended to bidirectional
    iterators, but this version doesn't take that step.
    
    The demo program that comes with it demonstrates sorting a vector such that
    the first half of the sorted elements end up on even indices in increasing
    order and the second half of sorted elements end up on odd indices in
    decreasing order.  The sort just requires std::sort with no post-copy or
    post-permute.

---
+++ folded_interleave_sort.cc
@@ -0,0 +1,70 @@
+#include <algorithm>
+#include <cstddef>
+#include <iostream>
+#include <random>
+
+#include "projection_iterator.hh"
+
+namespace {
+
+using std::ptrdiff_t;
+using jz::make_projection_iterator;
+
+// Returns a projection function for a particular size random-access container
+// such that this view through the projection:
+//
+//      0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+// 
+// is laid out in memory as:
+// 
+//      0, 9, 1, 8, 2, 7, 3, 6, 4, 5
+auto make_folded_interleave_projection(ptrdiff_t size) {
+  return [size](const ptrdiff_t index) {
+    const auto index2 = 2 * index;
+    return index2 >= size ? 2*size - index2 - 1 : index2; 
+  };
+}
+
+// Prints a span of values on stdout, separated by commas.
+template <typename Iter>
+void print_span(Iter first, Iter last) {
+
+  for (Iter it = first; it != last; ++it) {
+    if (it != first) {
+      std::cout << ", ";
+    }
+    std::cout << *it;
+  }
+}
+
+}  // namespace
+
+
+int main() {
+  std::random_device rd;
+  std::mt19937_64 g(rd());
+  std::vector<int> v{};
+
+  for (int i = 0; i < 15; ++i) {
+    v.push_back(i);
+    std::shuffle(v.begin(), v.end(), g);
+
+    std::cout << "Before:   ";
+    print_span(v.cbegin(), v.cend());
+    std::cout << '\n';
+
+    auto fip_proj  = make_folded_interleave_projection(v.size());
+    auto fip_begin = make_projection_iterator(v.begin(), fip_proj);
+    auto fip_end   = fip_begin + v.size();
+
+    std::sort(fip_begin, fip_end);
+
+    std::cout << "After:    ";
+    print_span(v.cbegin(), v.cend());
+    std::cout << '\n';
+
+    std::cout << "FIP view: ";
+    print_span(fip_begin, fip_end);
+    std::cout << "\n\n";
+  }
+}
---
+++ projection_iterator.hh
@@ -0,0 +1,178 @@
+#ifndef PROJECTION_ITERATOR_HH_
+#define PROJECTION_ITERATOR_HH_
+
+#if __cplusplus < 201400L
+# error "Requires C++14 or newer."
+#endif
+
+#include <cstddef>
+#include <iterator>
+#include <type_traits>
+
+namespace jz {
+
+// Returns an iterator representing a projection applied to the container that
+// the provided base iterator points into.  The base iterator must be a random
+// access iterator.  The returned iterator is also a random access iterator.
+//
+// The projection() callable must accept an index of type ptrdiff_t relative to
+// base, and return the projected index as an integral type.  It's up to the
+// caller to provide a projection function whose returned values stay within
+// the container relative to the base iterator.
+template <typename Iter, typename Callable>
+constexpr inline auto make_projection_iterator(
+    Iter base, Callable& projection) noexcept {
+  using std::ptrdiff_t;
+
+  static_assert(
+    std::is_base_of<
+      std::random_access_iterator_tag,
+      typename std::iterator_traits<Iter>::iterator_category>::value,
+      "Must use a random access iterator.");
+
+#if __cplusplus >= 201703L
+  static_assert(
+    std::is_integral<std::invoke_result_t<Callable, ptrdiff_t>>::value,
+    "Projection callable must return an integral type.");
+#elif __cplusplus >= 201100L
+  static_assert(
+    std::is_integral<typename std::result_of<Callable(ptrdiff_t)>::type>::value,
+    "Projection callable must return an integral type.");
+#endif
+  using IterTraits = typename std::iterator_traits<Iter>;
+
+  // The projection iterator.  Calls projection(index) to project a virtual
+  // index into an actual index applied to the base iterator.
+  class ProjIter {
+   public:
+    explicit constexpr ProjIter(Iter base, Callable& projection) noexcept
+    : base_{std::move(base)}, projection_{&projection}, index_{0} {}
+
+    ProjIter(const ProjIter&) = default;
+    ProjIter(ProjIter&&) = default;
+    ProjIter& operator=(const ProjIter&) = default;
+    ProjIter& operator=(ProjIter&&) = default;
+    ~ProjIter() = default;
+
+    // Dereference returns an lvalue reference to the element.
+    constexpr auto& operator*() const noexcept {
+      return *projected_();
+    }
+
+    // Returns the original iterator offset to the correct point.
+    constexpr auto operator->() const noexcept {
+      return projected_();
+    }
+
+    // Increments our index relative to first.
+    constexpr auto& operator++() noexcept {
+      ++index_;
+      return *this;
+    }
+
+    constexpr auto operator++(int) noexcept {
+      auto copy = *this;
+      this->operator++();
+      return copy;
+    }
+
+    // Decrements our index relative to first.
+    constexpr auto& operator--() noexcept {
+      --index_;
+      return *this;
+    }
+
+    constexpr auto operator--(int) noexcept {
+      auto copy = *this;
+      this->operator--();
+      return copy;
+    }
+
+    // Adds to our iterator.
+    constexpr auto& operator+=(const ptrdiff_t delta) noexcept {
+      index_ += delta;
+      return *this;
+    }
+
+    constexpr auto operator+(const ptrdiff_t delta) const noexcept {
+      auto copy = *this;
+      copy += delta;
+      return copy;
+    }
+
+    // Subtracts from our iterator.
+    constexpr auto& operator-=(const ptrdiff_t delta) noexcept {
+      index_ += delta;
+      return *this;
+    }
+
+    constexpr auto operator-(const ptrdiff_t delta) const noexcept {
+      auto copy = *this;
+      copy -= delta;
+      return copy;
+    }
+
+    // Returns difference between two iterators.
+    constexpr auto operator-(const ProjIter& rhs) const noexcept {
+      return index_ - rhs.index_;
+    }
+
+    // Base comparisons.  Note, these don't check whether you're comparing
+    // dissimilar iterators (e.g. different base iterators).
+    constexpr bool operator<(const ProjIter& rhs) const noexcept {
+      return index_ < rhs.index_;
+    }
+
+    constexpr bool operator==(const ProjIter& rhs) const noexcept {
+      return index_ == rhs.index_;
+    }
+
+    // Derived comparisons.
+    constexpr bool operator>(const ProjIter& rhs) const noexcept {
+      return rhs < *this;
+    }
+
+    constexpr bool operator<=(const ProjIter& rhs) const noexcept {
+      return !(*this > rhs);
+    }
+
+    constexpr bool operator>=(const ProjIter& rhs) const noexcept {
+      return !(*this < rhs);
+    }
+
+    constexpr bool operator!=(const ProjIter& rhs) const noexcept {
+      return !(*this == rhs);
+    }
+
+    using iterator_category = std::random_access_iterator_tag;
+    using difference_type   = typename IterTraits::difference_type;
+    using value_type        = typename IterTraits::value_type;
+    using pointer           = typename IterTraits::pointer;
+    using reference         = typename IterTraits::reference;
+
+   private:
+    Iter base_;
+    Callable* projection_;
+    ptrdiff_t index_;
+
+    // Returns the advanced pointer.  While we could do this with a simple
+    // addition, std::advance opens the possibility we include other iterator
+    // categories eventually.
+    constexpr Iter projected_() const {
+      Iter copy = base_;
+      std::advance(copy, (*projection_)(index_));
+      return copy;
+    }
+
+    // Supresses meaningless warnings about these types not being used.
+    // They are actually used, but G++'s warning gets it wrong.
+    void unused_(iterator_category, difference_type, value_type, pointer,
+                 reference) {}
+  };
+
+  return ProjIter(base, projection);
+}
+
+}  // namespace jz
+
+#endif // PROJECTION_ITERATOR_HH_
